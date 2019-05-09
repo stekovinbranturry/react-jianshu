7256ee6bb84ef65f6a3dae34a920cc797d195c84
Zhang Kai <zhangkai930418@gmail.com>
2019-05-09 16:19:10 +0800

    add onclick event to toggle menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,84 +1,108 @@
-import React from 'react';
+import React, { Component } from 'react';
 import navLogo from '../image/nav-logo.png';
 import navBeta from '../image/nav-beta.png';
 
-const Header = () => {
-	return (
-		<nav className="navbar navbar-default navbar-fixed-top">
-			<div className="width-limit">
-				<a className="logo" href="/">
-					<img src={navLogo} alt="Nav logo" />
-				</a>
-
-				<a className="btn write-btn" target="_blank" href="/">
-					<i className="iconfont ic-write" />
-					写文章
-				</a>
-				<a className="btn sign-up" id="sign_up" href="/">
-					注册
-				</a>
-				<a className="btn log-in" id="sign_in" href="/">
-					登录
-				</a>
-
-				<div className="beta">
-					<a href="/">
-						<img src={navBeta} alt="beta" />
+export default class Header extends Component {
+	constructor() {
+		super();
+		this.state = {
+			isMenuShow: false,
+			menuClassName: 'collapse navbar-collapse'
+		};
+	}
+	handleClickOnNavbarButton = () => {
+		this.setState(() => {
+			return this.state.isMenuShow
+				? {
+						menuClassName: 'collapse navbar-collapse',
+						isMenuShow: false
+				  }
+				: {
+						menuClassName: 'collapse navbar-collapse in',
+						isMenuShow: true
+				  };
+		});
+	};
+	render() {
+		return (
+			<nav className="navbar navbar-default navbar-fixed-top">
+				<div className="width-limit">
+					<a className="logo" href="/">
+						<img src={navLogo} alt="Nav logo" />
 					</a>
-				</div>
 
-				<div className="style-mode">
-					<a className="style-mode-btn" href="/">
-						<i className="iconfont ic-navigation-mode" />
+					<a className="btn write-btn" target="_blank" href="/">
+						<i className="iconfont ic-write" />
+						写文章
 					</a>
-				</div>
-				<div className="container">
-					<div className="navbar-header">
-						<button type="button" className="navbar-toggle collapsed">
-							<span className="icon-bar" />
-							<span className="icon-bar" />
-							<span className="icon-bar" />
-						</button>
+					<a className="btn sign-up" id="sign_up" href="/">
+						注册
+					</a>
+					<a className="btn log-in" id="sign_in" href="/">
+						登录
+					</a>
+
+					<div className="beta">
+						<a href="/">
+							<img src={navBeta} alt="beta" />
+						</a>
 					</div>
-					<div className="collapse navbar-collapse" id="menu">
-						<ul className="nav navbar-nav">
-							<li className="tab active">
-								<a href="/">
-									<span className="menu-text">首页</span>
-									<i className="iconfont ic-navigation-discover menu-icon" />
-								</a>
-							</li>
-							<li className="tab ">
-								<a
-									id="web-nav-app-download-btn"
-									className="app-download-btn"
-									href="/apps?utm_medium=desktop&amp;utm_source=navbar-apps"
-								>
-									<span className="menu-text">下载App</span>
-									<i className="iconfont ic-navigation-download menu-icon" />
-								</a>
-							</li>
-							<li className="search">
-								<form target="_blank" action="/search" method="get">
-									<input name="utf8" type="hidden" value="✓" />
-									<input
-										type="text"
-										name="q"
-										id="q"
-										placeholder="搜索"
-										className="search-input"
-									/>
-									<a className="search-btn" href="/">
-										<i className="iconfont ic-search" />
+
+					<div className="style-mode">
+						<a className="style-mode-btn" href="/">
+							<i className="iconfont ic-navigation-mode" />
+						</a>
+					</div>
+					<div className="container">
+						<div className="navbar-header">
+							<button
+								type="button"
+								className="navbar-toggle collapsed"
+								onClick={this.handleClickOnNavbarButton}
+							>
+								<span className="icon-bar" />
+								<span className="icon-bar" />
+								<span className="icon-bar" />
+							</button>
+						</div>
+						<div className={this.state.menuClassName} id="menu">
+							<ul className="nav navbar-nav">
+								<li className="tab active">
+									<a href="/">
+										<span className="menu-text">首页</span>
+										<i className="iconfont ic-navigation-discover menu-icon" />
 									</a>
-								</form>
-							</li>
-						</ul>
+								</li>
+								<li className="tab ">
+									<a
+										id="web-nav-app-download-btn"
+										className="app-download-btn"
+										href="/apps?utm_medium=desktop&amp;utm_source=navbar-apps"
+									>
+										<span className="menu-text">下载App</span>
+										<i className="iconfont ic-navigation-download menu-icon" />
+									</a>
+								</li>
+								<li className="search">
+									<form target="_blank" action="/search" method="get">
+										<input name="utf8" type="hidden" value="✓" />
+										<input
+											type="text"
+											name="q"
+											id="q"
+											placeholder="搜索"
+											className="search-input"
+										/>
+										<a className="search-btn" href="/">
+											<i className="iconfont ic-search" />
+										</a>
+									</form>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
-			</div>
-		</nav>
-	);
-};
-
-export default Header;
+			</nav>
+		);
+	}
+}
