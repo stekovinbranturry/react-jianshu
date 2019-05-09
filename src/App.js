79c1f54a7d83e6bcfbc4d10e84413440845bbbd3
Zhang Kai <zhangkai930418@gmail.com>
2019-05-09 09:59:18 +0800

    create header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-
-function App() {
-	return <div>hello</div>;
-}
+import Header from './components/Header';
+const App = () => {
+	return (
+		<div>
+			<Header />
+		</div>
+	);
+};
 
 export default App;
