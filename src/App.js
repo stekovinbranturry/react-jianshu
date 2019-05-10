642f426b0861aed30f6579a690ba2db11ed3505e
Zhang Kai <zhangkai930418@gmail.com>
2019-05-10 17:06:31 +0800

    add Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
+
 const App = () => {
 	return (
 		<Fragment>
 			<Header />
+			<Footer />
 		</Fragment>
 	);
 };
