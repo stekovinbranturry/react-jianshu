6e6adbda51feecd7bf12b5b74276d3001542c5b6
Zhang Kai <zhangkai930418@gmail.com>
2019-05-10 19:35:33 +0800

    seperate into small components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import navLogo from '../image/nav-logo.png';
 import navBeta from '../image/nav-beta.png';
@@ -6,13 +6,18 @@ import { createToggleMenuAction } from '../store/action';
 
 const Header = props => {
 	const { menuClassName, handleClickOnNavbarButton } = props;
-	return (
-		<nav className="navbar navbar-default navbar-fixed-top">
-			<div className="width-limit">
-				<a className="logo" href="/">
-					<img src={navLogo} alt="Nav logo" />
-				</a>
 
+	const Logo = () => {
+		return (
+			<a className="logo" href="/">
+				<img src={navLogo} alt="Nav logo" />
+			</a>
+		);
+	};
+
+	const User = () => {
+		return (
+			<Fragment>
 				<a
 					className="btn write-btn"
 					target="_blank"
@@ -28,71 +33,102 @@ const Header = props => {
 				<a className="btn log-in" id="sign_in" href="/">
 					登录
 				</a>
+			</Fragment>
+		);
+	};
 
-				<div className="beta">
-					<a href="/">
-						<img src={navBeta} alt="beta" />
-					</a>
-				</div>
-
-				<div className="style-mode">
-					<a className="style-mode-btn" href="/">
-						<i className="iconfont ic-navigation-mode" />
-					</a>
-				</div>
-				<div className="container">
-					<div className="navbar-header">
-						<button
-							type="button"
-							className="navbar-toggle collapsed"
-							onClick={handleClickOnNavbarButton}
+	const Menu = () => {
+		return (
+			<div className={menuClassName} id="menu">
+				<ul className="nav navbar-nav">
+					<li className="tab active">
+						<a href="/">
+							<span className="menu-text">首页</span>
+							<i className="iconfont ic-navigation-discover menu-icon" />
+						</a>
+					</li>
+					<li className="tab ">
+						<a
+							id="web-nav-app-download-btn"
+							className="app-download-btn"
+							href="/apps?utm_medium=desktop&amp;utm_source=navbar-apps"
 						>
-							<span className="icon-bar" />
-							<span className="icon-bar" />
-							<span className="icon-bar" />
-						</button>
-					</div>
-					<div className={menuClassName} id="menu">
-						<ul className="nav navbar-nav">
-							<li className="tab active">
-								<a href="/">
-									<span className="menu-text">首页</span>
-									<i className="iconfont ic-navigation-discover menu-icon" />
-								</a>
-							</li>
-							<li className="tab ">
-								<a
-									id="web-nav-app-download-btn"
-									className="app-download-btn"
-									href="/apps?utm_medium=desktop&amp;utm_source=navbar-apps"
-								>
-									<span className="menu-text">下载App</span>
-									<i className="iconfont ic-navigation-download menu-icon" />
-								</a>
-							</li>
-							<li className="search">
-								<form
-									target="_blank"
-									rel="noopener noreferrer"
-									action="/search"
-									method="get"
-								>
-									<input name="utf8" type="hidden" value="✓" />
-									<input
-										type="text"
-										name="q"
-										id="q"
-										placeholder="搜索"
-										className="search-input"
-									/>
-									<a className="search-btn" href="/">
-										<i className="iconfont ic-search" />
-									</a>
-								</form>
-							</li>
-						</ul>
-					</div>
+							<span className="menu-text">下载App</span>
+							<i className="iconfont ic-navigation-download menu-icon" />
+						</a>
+					</li>
+					<li className="search">
+						<form
+							target="_blank"
+							rel="noopener noreferrer"
+							action="/search"
+							method="get"
+						>
+							<input name="utf8" type="hidden" value="✓" />
+							<input
+								type="text"
+								name="q"
+								id="q"
+								placeholder="搜索"
+								className="search-input"
+							/>
+							<a className="search-btn" href="/">
+								<i className="iconfont ic-search" />
+							</a>
+						</form>
+					</li>
+				</ul>
+			</div>
+		);
+	};
+
+	const Beta = () => {
+		return (
+			<div className="beta">
+				<a href="/">
+					<img src={navBeta} alt="beta" />
+				</a>
+			</div>
+		);
+	};
+
+	const NavigationMode = () => {
+		return (
+			<div className="style-mode">
+				<a className="style-mode-btn" href="/">
+					<i className="iconfont ic-navigation-mode" />
+				</a>
+			</div>
+		);
+	};
+
+	const NavbarButton = () => {
+		return (
+			<div className="container">
+				<div className="navbar-header">
+					<button
+						type="button"
+						className="navbar-toggle collapsed"
+						onClick={handleClickOnNavbarButton}
+					>
+						<span className="icon-bar" />
+						<span className="icon-bar" />
+						<span className="icon-bar" />
+					</button>
 				</div>
+				<Menu />
+			</div>
+		);
+	};
+
+	return (
+		<nav className="navbar navbar-default navbar-fixed-top">
+			<div className="width-limit">
+				<Logo />
+				<User />
+				<Beta />
+				<NavigationMode />
+				<NavbarButton />
 			</div>
 		</nav>
 	);
