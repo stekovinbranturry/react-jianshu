b3f3bb648b0a3b3370d1ffa223c7bfb402ae1e65
Zhang Kai <zhangkai930418@gmail.com>
2019-05-14 21:37:54 +0800

    add home/topic component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import Carousel from './homeComponents/Carousel';
 import ArticleList from './homeComponents/ArticleList';
+import Topic from './homeComponents/Topic';
 import Board from './homeComponents/Board';
 import Qrbox from './homeComponents/Qrbox';
 import Author from './homeComponents/Author';
@@ -8,11 +9,20 @@ import Author from './homeComponents/Author';
 const Home = () => {
 	return (
 		<Fragment>
-			<Carousel />
-			<ArticleList />
-			<Board />
-			<Qrbox />
-			<Author />
+			<div class="container index">
+				<div class="row">
+					<div class="col-xs-16 main">
+						<Carousel />
+						<Topic />
+						<ArticleList />
+					</div>
+					<div class="col-xs-7 col-xs-offset-1 aside">
+						<Board />
+						<Qrbox />
+						<Author />
+					</div>
+				</div>
+			</div>
 		</Fragment>
 	);
 };
