b5d292b37c57468f89bea60cedaf842bfdb3c660
Zhang Kai <zhangkai930418@gmail.com>
2019-05-14 21:41:24 +0800

    Topic details

--- src/components/home/homeComponents/Topic.js
+++ src/components/home/homeComponents/Topic.js
@@ -1,7 +1,102 @@
 import React from 'react';
 
 const Topic = () => {
-	return <div>Topic</div>;
+	return (
+		<div className="recommend-collection">
+			<a
+				className="collection"
+				target="_blank"
+				rel="noopener noreferrer"
+				href="/c/yD9GAd?utm_medium=index-collections&amp;utm_source=desktop"
+			>
+				<img
+					src="//upload.jianshu.io/collections/images/4/sy_20091020135145113016.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64"
+					alt="64"
+				/>
+				<div className="name">读书</div>
+			</a>
+			<a
+				className="collection"
+				target="_blank"
+				rel="noopener noreferrer"
+				href="/c/8c92f845cd4d?utm_medium=index-collections&amp;utm_source=desktop"
+			>
+				<img
+					src="//upload.jianshu.io/collections/images/283250/%E6%BC%AB%E7%94%BB%E4%B8%93%E9%A2%98.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64"
+					alt="64"
+				/>
+				<div className="name">手绘</div>
+			</a>
+			<a
+				className="collection"
+				target="_blank"
+				rel="noopener noreferrer"
+				href="/c/7b2be866f564?utm_medium=index-collections&amp;utm_source=desktop"
+			>
+				<img
+					src="//upload.jianshu.io/collections/images/83/1.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64"
+					alt="64"
+				/>
+				<div className="name">摄影</div>
+			</a>
+			<a
+				className="collection"
+				target="_blank"
+				rel="noopener noreferrer"
+				href="/c/5AUzod?utm_medium=index-collections&amp;utm_source=desktop"
+			>
+				<img
+					src="//upload.jianshu.io/collections/images/13/%E5%95%8A.png?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64"
+					alt="64"
+				/>
+				<div className="name">旅行·在路上</div>
+			</a>
+			<a
+				className="collection"
+				target="_blank"
+				rel="noopener noreferrer"
+				href="/c/1hjajt?utm_medium=index-collections&amp;utm_source=desktop"
+			>
+				<img
+					src="//upload.jianshu.io/collections/images/21/20120316041115481.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64"
+					alt="64"
+				/>
+				<div className="name">简书电影</div>
+			</a>
+			<a
+				className="collection"
+				target="_blank"
+				rel="noopener noreferrer"
+				href="/c/fcd7a62be697?utm_medium=index-collections&amp;utm_source=desktop"
+			>
+				<img
+					src="//upload.jianshu.io/collections/images/95/1.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64"
+					alt="64"
+				/>
+				<div className="name">故事</div>
+			</a>
+			<a
+				className="collection"
+				target="_blank"
+				rel="noopener noreferrer"
+				href="/c/V2CqjW?utm_medium=index-collections&amp;utm_source=desktop"
+			>
+				<img
+					src="//upload.jianshu.io/collections/images/14/6249340_194140034135_2.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64"
+					alt="64"
+				/>
+				<div className="name">@IT·互联网</div>
+			</a>
+			<a
+				className="more-hot-collection"
+				target="_blank"
+				rel="noopener noreferrer"
+				href="/recommendations/collections?utm_medium=index-collections&amp;utm_source=desktop"
+			>
+				更多热门专题 <i className="iconfont ic-link" />
+			</a>
+		</div>
+	);
 };
 
 export default Topic;
