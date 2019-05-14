33cb9b0c9fbbe4f0691e34602f7b95d88a1f3823
Zhang Kai <zhangkai930418@gmail.com>
2019-05-14 22:22:10 +0800

    Author component details

--- src/components/home/homeComponents/Author.js
+++ src/components/home/homeComponents/Author.js
@@ -1,7 +1,165 @@
 import React from 'react';
 
 const Author = () => {
-	return <div>Author</div>;
+	return (
+		<div className="recommended-author-wrap">
+			<div className="recommended-authors">
+				<div className="title">
+					<span>推荐作者</span>
+					<button className="page-change">
+						<i className="iconfont ic-search-change" />
+						换一批
+					</button>
+				</div>
+				<ul className="list">
+					<li>
+						<a
+							href="/u/0f438ff0a55f?utm_source=desktop&amp;utm_medium=index-users"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="avatar"
+						>
+							<img
+								src="//upload.jianshu.io/users/upload_avatars/4790772/388e473c-fe2f-40e0-9301-e357ae8f1b41.jpeg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp"
+								alt="avatar"
+							/>
+						</a>
+
+						<a href="https://www.jianshu.com/" className="follow" state="0">
+							<i className="iconfont ic-follow" />
+							关注
+						</a>
+						<a
+							href="/u/0f438ff0a55f?utm_source=desktop&amp;utm_medium=index-users"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="name"
+						>
+							茶点故事
+						</a>
+						<p>写了240.6k字 · 8.2k喜欢</p>
+					</li>
+					<li>
+						<a
+							href="/u/c4165d16d0ad?utm_source=desktop&amp;utm_medium=index-users"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="avatar"
+						>
+							<img
+								src="//upload.jianshu.io/users/upload_avatars/3292545/78f2855a-80fd-419a-9fb2-1aeed39690f0.png?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp"
+								alt="avatar"
+							/>
+						</a>
+
+						<a href="https://www.jianshu.com/" className="follow" state="0">
+							<i className="iconfont ic-follow" />
+							关注
+						</a>
+						<a
+							href="/u/c4165d16d0ad?utm_source=desktop&amp;utm_medium=index-users"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="name"
+						>
+							北美之北
+						</a>
+						<p>写了404.4k字 · 28.8k喜欢</p>
+					</li>
+					<li>
+						<a
+							href="/u/757b5f9f910b?utm_source=desktop&amp;utm_medium=index-users"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="avatar"
+						>
+							<img
+								src="//upload.jianshu.io/users/upload_avatars/12665193/828a449f-efbb-4540-b0af-f6c314810515.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp"
+								alt="avatar"
+							/>
+						</a>
+
+						<a href="https://www.jianshu.com/" className="follow" state="0">
+							<i className="iconfont ic-follow" />
+							关注
+						</a>
+						<a
+							href="/u/757b5f9f910b?utm_source=desktop&amp;utm_medium=index-users"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="name"
+						>
+							宇文歡
+						</a>
+						<p>写了283.2k字 · 2.4k喜欢</p>
+					</li>
+					<li>
+						<a
+							href="/u/4062aaeba322?utm_source=desktop&amp;utm_medium=index-users"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="avatar"
+						>
+							<img
+								src="//upload.jianshu.io/users/upload_avatars/7290998/f64f5ef0-def0-4b26-beb3-b9d88f060ba0.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp"
+								alt="avatar"
+							/>
+						</a>
+
+						<a href="https://www.jianshu.com/" className="follow" state="0">
+							<i className="iconfont ic-follow" />
+							关注
+						</a>
+						<a
+							href="/u/4062aaeba322?utm_source=desktop&amp;utm_medium=index-users"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="name"
+						>
+							念远怀人
+						</a>
+						<p>写了625.4k字 · 12.4k喜欢</p>
+					</li>
+					<li>
+						<a
+							href="/u/55b597320c4e?utm_source=desktop&amp;utm_medium=index-users"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="avatar"
+						>
+							<img
+								src="//upload.jianshu.io/users/upload_avatars/1835526/83d24e1a-0a0f-43f6-8a1d-289be6101e73.png?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp"
+								alt="avatar"
+							/>
+						</a>
+
+						<a href="https://www.jianshu.com/" className="follow" state="0">
+							<i className="iconfont ic-follow" />
+							关注
+						</a>
+						<a
+							href="/u/55b597320c4e?utm_source=desktop&amp;utm_medium=index-users"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="name"
+						>
+							行距版君
+						</a>
+						<p>写了492.6k字 · 53.3k喜欢</p>
+					</li>
+				</ul>
+				<a
+					href="/recommendations/users?utm_source=desktop&amp;utm_medium=index-users"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="find-more"
+				>
+					查看全部
+					<i className="iconfont ic-link" />
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default Author;
