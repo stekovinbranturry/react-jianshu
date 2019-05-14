1e37144f4d608e4deb87dc41e9a49eaa15ffad87
Zhang Kai <zhangkai930418@gmail.com>
2019-05-14 14:32:49 +0800

    splite header ui, make code easier to read

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,13 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import navLogo from '../../image/nav-logo.png';
-import navBeta from '../../image/nav-beta.png';
 import * as headerActions from './headerActions';
+import HeaderUIComponents from './HeaderUIComponents';
 
 class Header extends Component {
 	componentDidMount() {
 		this.props.handleChangeSearchTips();
 	}
+
 	render() {
 		const {
 			menuClassName,
@@ -16,170 +16,13 @@ class Header extends Component {
 			handleChangeSearchTips
 		} = this.props;
 
-		const Logo = () => {
-			return (
-				<a className="logo" href="/">
-					<img src={navLogo} alt="Nav logo" />
-				</a>
-			);
-		};
-
-		const User = () => {
-			return (
-				<Fragment>
-					<a
-						className="btn write-btn"
-						target="_blank"
-						rel="noopener noreferrer"
-						href="/"
-					>
-						<i className="iconfont ic-write" />
-						写文章
-					</a>
-					<a className="btn sign-up" id="sign_up" href="/">
-						注册
-					</a>
-					<a className="btn log-in" id="sign_in" href="/">
-						登录
-					</a>
-				</Fragment>
-			);
-		};
-
-		const SearchTips = () => {
-			return (
-				<div id="navbar-search-tips">
-					<div className="search-trending">
-						<div className="search-trending-header clearfix">
-							<span>热门搜索</span>
-							<a onClick={handleChangeSearchTips}>
-								<i className="iconfont ic-search-change" />
-								换一批
-							</a>
-						</div>
-						<ul className="search-trending-tag-wrap">
-							{searchTipsList.map((item, index) => {
-								return (
-									<li key={index}>
-										<a
-											href={
-												'https://www.jianshu.com/search?q=' +
-												item +
-												'&page=1&type=note'
-											}
-										>
-											{item}
-										</a>
-									</li>
-								);
-							})}
-						</ul>
-					</div>
-				</div>
-			);
-		};
-
-		const Search = () => {
-			return (
-				<li className="search">
-					<form
-						target="_blank"
-						rel="noopener noreferrer"
-						action="/search"
-						method="get"
-					>
-						<input name="utf8" type="hidden" value="✓" />
-						<input
-							type="text"
-							name="q"
-							id="q"
-							placeholder="搜索"
-							className="search-input"
-						/>
-						<a className="search-btn" href="/">
-							<i className="iconfont ic-search" />
-						</a>
-						<SearchTips />
-					</form>
-				</li>
-			);
-		};
-
-		const Menu = () => {
-			return (
-				<div className={menuClassName} id="menu">
-					<ul className="nav navbar-nav">
-						<li className="tab active">
-							<a href="/">
-								<span className="menu-text">首页</span>
-								<i className="iconfont ic-navigation-discover menu-icon" />
-							</a>
-						</li>
-						<li className="tab ">
-							<a
-								id="web-nav-app-download-btn"
-								className="app-download-btn"
-								href="/apps?utm_medium=desktop&amp;utm_source=navbar-apps"
-							>
-								<span className="menu-text">下载App</span>
-								<i className="iconfont ic-navigation-download menu-icon" />
-							</a>
-						</li>
-						<Search />
-					</ul>
-				</div>
-			);
-		};
-
-		const Beta = () => {
-			return (
-				<div className="beta">
-					<a href="/">
-						<img src={navBeta} alt="beta" />
-					</a>
-				</div>
-			);
-		};
-
-		const NavigationMode = () => {
-			return (
-				<div className="style-mode">
-					<a className="style-mode-btn" href="/">
-						<i className="iconfont ic-navigation-mode" />
-					</a>
-				</div>
-			);
-		};
-
-		const NavbarButton = () => {
-			return (
-				<div className="container">
-					<div className="navbar-header">
-						<button
-							type="button"
-							className="navbar-toggle collapsed"
-							onClick={handleClickOnNavbarButton}
-						>
-							<span className="icon-bar" />
-							<span className="icon-bar" />
-							<span className="icon-bar" />
-						</button>
-					</div>
-					<Menu />
-				</div>
-			);
-		};
-
 		return (
-			<nav className="navbar navbar-default navbar-fixed-top">
-				<div className="width-limit">
-					<Logo />
-					<User />
-					<Beta />
-					<NavigationMode />
-					<NavbarButton />
-				</div>
-			</nav>
+			<HeaderUIComponents
+				menuClassName={menuClassName}
+				handleClickOnNavbarButton={handleClickOnNavbarButton}
+				searchTipsList={searchTipsList}
+				handleChangeSearchTips={handleChangeSearchTips}
+			/>
 		);
 	}
 }
