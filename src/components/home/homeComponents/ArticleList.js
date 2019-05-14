3747e387352f034f341b6bb1c1370ba07de5bfd6
Zhang Kai <zhangkai930418@gmail.com>
2019-05-14 21:46:33 +0800

    Article component details

--- src/components/home/homeComponents/ArticleList.js
+++ src/components/home/homeComponents/ArticleList.js
@@ -1,7 +1,110 @@
 import React from 'react';
 
 const ArticleList = () => {
-	return <div>ArticleList</div>;
+	return (
+		<div className="split-line">
+			<div id="list-container">
+				<ul className="note-list" infinite-scroll-url="/">
+					<li id="note-41856914" data-note-id="41856914" className="have-img">
+						<a className="wrap-img" href="/p/6383eb3ad99b" target="_blank">
+							<img
+								className="  img-blur-done"
+								src="//upload-images.jianshu.io/upload_images/2843129-5591608f584647d8.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240"
+								alt="120"
+							/>
+						</a>
+						<div className="content">
+							<a className="title" target="_blank" href="/p/6383eb3ad99b">
+								这个已被日本主妇用到极致的大嘴盒，你居然还没听过
+							</a>
+							<p className="abstract">
+								在日本，河马口收纳盒早就受到了日本主妇的热爱，因其对空间完全无挑剔，容量又特别大，对面积有限的屋子来说，实在友好。
+								越是小家越怕乱，一乱就显得无...
+							</p>
+							<div className="meta">
+								<span className="jsd-meta">
+									<i className="iconfont ic-paid1" /> 2.6
+								</span>
+								<a className="nickname" target="_blank" href="/u/f1f93146cae8">
+									收了纳个Queen
+								</a>
+								<a target="_blank" href="/p/6383eb3ad99b#comments">
+									<i className="iconfont ic-list-comments" /> 4
+								</a>
+								<span>
+									<i className="iconfont ic-list-like" /> 18
+								</span>
+							</div>
+						</div>
+					</li>
+					<li id="note-38946839" data-note-id="38946839" className="have-img">
+						<a className="wrap-img" href="/p/8deabf509ca3" target="_blank">
+							<img
+								className="  img-blur-done"
+								src="//upload-images.jianshu.io/upload_images/2122505-4a0c9b9074856ce9.png?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240"
+								alt="120"
+							/>
+						</a>
+						<div className="content">
+							<a className="title" target="_blank" href="/p/8deabf509ca3">
+								Flutter 与 iOS 原生 webView 对比
+							</a>
+							<p className="abstract">
+								本文对比的是
+								UIWebView、WKWebView、flutter_webview_plugin（在iOS中使用的是WKWebView）的加载...
+							</p>
+							<div className="meta">
+								<span className="jsd-meta">
+									<i className="iconfont ic-paid1" /> 2.1
+								</span>
+								<a className="nickname" target="_blank" href="/u/eca4b14ce9fc">
+									雨天__心情
+								</a>
+								<a target="_blank" href="/p/8deabf509ca3#comments">
+									<i className="iconfont ic-list-comments" /> 1
+								</a>
+								<span>
+									<i className="iconfont ic-list-like" /> 16
+								</span>
+							</div>
+						</div>
+					</li>
+					<li id="note-40822299" data-note-id="40822299" className="have-img">
+						<a className="wrap-img" href="/p/2dab51cf3ebd" target="_blank">
+							<img
+								className="img-blur-done"
+								src="//upload-images.jianshu.io/upload_images/15736285-b5ea5a40a953e6ac.png?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240"
+								alt="120"
+							/>
+						</a>
+						<div className="content">
+							<a className="title" target="_blank" href="/p/2dab51cf3ebd">
+								皮肤变白很困难？做到三点就很简单
+							</a>
+							<p className="abstract">
+								第一点：坚持防晒
+								防晒做好了才能够让我们的皮肤不加快老化，就能够抑制黑色素的形成，这样就能够从根源上解决很多因为色素沉淀导致的肌肤问题，皮肤黑就...
+							</p>
+							<div className="meta">
+								<a className="nickname" target="_blank" href="/u/f9723e250da3">
+									你是我的夏天i
+								</a>
+								<a target="_blank" href="/p/2dab51cf3ebd#comments">
+									<i className="iconfont ic-list-comments" /> 0
+								</a>
+								<span>
+									<i className="iconfont ic-list-like" /> 7
+								</span>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<a data-page="3" href="/" className="load-more">
+				阅读更多
+			</a>
+		</div>
+	);
 };
 
 export default ArticleList;
