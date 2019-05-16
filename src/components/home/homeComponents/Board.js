455336f07cf6a29f68417300f9f1c42728b0766c
Zhang Kai <zhangkai930418@gmail.com>
2019-05-16 14:53:07 +0800

    optimize

--- src/components/home/homeComponents/Board.js
+++ src/components/home/homeComponents/Board.js
@@ -7,14 +7,19 @@ import bannerLecture from '../../../image/banner-s-lecture.png';
 const Board = () => {
 	return (
 		<div className="board">
-			<a target="_blank" rel="noopener noreferrer" href="/mobile/club">
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href="https://www.jianshu.com/mobile/club"
+			>
 				<img src={bannerBook} alt="Banner s club" />
 			</a>
 
 			<a
 				utm_medium="index-banner-s"
 				target="_blank"
-				href="/mobile/books?category_id=284"
+				rel="noopener noreferrer"
+				href="https://www.jianshu.com/mobile/books?category_id=284"
 			>
 				<img src={bannerClub} alt="Banner s club" />
 			</a>
@@ -23,14 +28,15 @@ const Board = () => {
 				utm_medium="index-banner-s"
 				target="_blank"
 				rel="noopener noreferrer"
-				href="/publications"
+				href="https://www.jianshu.com/publications"
 			>
 				<img src={bannerCopyright} alt="Banner s copyright" />
 			</a>
 
 			<a
 				target="_blank"
-				href="/c/e048f1a72e3d?utm_medium=index-banner-s&amp;utm_source=desktop"
+				rel="noopener noreferrer"
+				href="https://www.jianshu.com/c/e048f1a72e3d?utm_medium=index-banner-s&amp;utm_source=desktop"
 			>
 				<img src={bannerLecture} alt="Banner s lecture" />
 			</a>
