665b724131491133dd5319b5f15267e8f1725b60
Zhang Kai <zhangkai930418@gmail.com>
2019-05-20 15:55:54 +0800

    united coding style

--- src/components/header/headerActions.js
+++ src/components/header/headerActions.js
@@ -11,14 +11,11 @@ export const [TOGGLE_MENU, CHANGE_SEARCH_TIPS, GET_SEARCH_TIPS] = [
 /*
  * action creaters
  */
-export const createToggleMenuAction = () => {
-	return { type: TOGGLE_MENU };
-};
+export const createToggleMenuAction = () => ({ type: TOGGLE_MENU });
 
-export const createGetSearchTipsAction = () => {
-	return { type: GET_SEARCH_TIPS };
-};
+export const createGetSearchTipsAction = () => ({ type: GET_SEARCH_TIPS });
 
-export const createChangeSearchTipsAction = list => {
-	return { type: CHANGE_SEARCH_TIPS, list: fromJS(list) };
-};
+export const createChangeSearchTipsAction = list => ({
+	type: CHANGE_SEARCH_TIPS,
+	list: fromJS(list)
+});
